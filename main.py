589ea804527f23d1b7c4532020b1f0ac6468750e
Sylesh Sathish Kumar <75116474+onlyuuu@users.noreply.github.com>
2023-07-20 09:42:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -157,7 +157,7 @@ with st.container():
     st.write("##")
 
     contact_form = """
-    <form action="https://formsubmit.co/201001111@rajalakshmi.edu.in" method="POST">
+    <form action="https://formsubmit.co/el/xuzowa" method="POST">
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
